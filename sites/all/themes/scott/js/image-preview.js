c67ebedbb84d0340130cbb85738cf3aa17843b72
scottomoffatt@gmail.com <scottibiscotti@smo.PK5001Z>
2016-11-22 16:44:45 -0800

    Thumb height correct

--- sites/all/themes/scott/js/image-preview.js
+++ sites/all/themes/scott/js/image-preview.js
@@ -2,15 +2,16 @@
 
 $(document).ready(function(){
 $('.view-thumbnail-gallery .view-content .views-row .views-field-field-image').on('click' , function(e){
-   var mywidth =  $('.view-thumbnail-gallery .views-row .views-field-field-image img')[0].getBoundingClientRect().height;
+   var mywidth =  $('.view-thumbnail-gallery .views-row .views-field-field-image img')[0].getBoundingClientRect().width;
+   var widthstart = $('.view-thumbnail-gallery .views-row .views-field-field-image img').width();
    $('.button-wrapper').css({"top":mywidth /1.6});
   // $('.owl-buttons').css({"top": mywidth /2});
   e.preventDefault();
     if ($(this).siblings('.slide-container').hasClass('open')){
     $(this).siblings('.slide-container').removeClass('open');
-   $(this).parent('.views-row').css({"background" : "none"}).siblings().css({"background" : "none"});
+   $(this).parent('.views-row').css({"background" : "none","height" : "auto"}).siblings().css({"background" : "none"});
   }  else {
-  $(this).parent('.views-row').css({"background" : "rgba(137,224,224,1)"}).siblings().css({"background" : "none"});
+  $(this).parent('.views-row').css({"background" : "rgba(137,224,224,1)","height" : widthstart}).siblings().css({"background" : "none"});
     $(this).siblings('.slide-container').toggleClass('open').parent().siblings().children().removeClass('open');
     $('html,body').animate({scrollTop:$(this).parent().parent().offset().top -54},2);  
   } 
@@ -42,7 +43,7 @@ $(window).on("navigate", function (event, data) {
   if (direction == 'back') {
      $('.shrink').removeClass('shrink').css({"margin-bottom":"0"});
      $('.open').removeClass('open');
-     $('.view-thumbnail-gallery .view-content .views-row').css({"background" : "none"});  
+     $('.view-thumbnail-gallery .view-content .views-row ').css({"background" : "none"});  
   }
   //if (direction == 'forward') {
     // do something else
@@ -53,14 +54,14 @@ $(document).keyup(function(event) {
     if(event.which === 27) {
       $('.shrink').removeClass('shrink').css({"margin-bottom":"0"});
       $('.open').removeClass('open');
-      $('.view-thumbnail-gallery .view-content .views-row').css({"background" : "none"});  
+      $('.view-thumbnail-gallery .view-content .views-row ').css({"background" : "none"});  
     }
 });
 $('.button').on('click', function(e){
   e.preventDefault();
        $('.shrink').removeClass('shrink').css({"margin-bottom":"0"});
      $('.open').removeClass('open');
-     $('.view-thumbnail-gallery .view-content .views-row').css({"background" : "none"});  
+     $('.view-thumbnail-gallery .view-content .views-row ').css({"background" : "none"});  
 
 });
 $(function(){
@@ -70,8 +71,25 @@ $(function(){
       $('.views-field-body').css({"top": windowHH /1.6});
       $('#header').css({"top": windowHH});
 });
+function matchHeights() {
+  $('.view-thumbnail-gallery .views-row').each(function() {
+    var elements = $('.view-thumbnail-gallery .views-row .views-field-field-image img').find('.height');
+    var height = 0;
+    elements.css('min-height','0px');
+    elements.each(function() {
+      height = $(this).height() > height ? $(this).height() : height;
+    });
+    elements.css('min-height',height+'px');
+  });
+};
 
+$(document).ready(function() {
+  matchHeights();
+});
 
+$(window).resize(function() {
+  matchHeights();
+});
 
   
 });
