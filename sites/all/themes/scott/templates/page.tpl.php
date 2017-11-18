d5c7c72caf08c579a1e762f650e64ff632238c5a
scottomoffatt@gmail.com <scottomoffatt@gmail.com>
2017-11-17 16:19:13 -0800

    fixes for site info in header
    Fix layout problems in for the site information moved to header

--- sites/all/themes/scott/templates/page.tpl.php
+++ sites/all/themes/scott/templates/page.tpl.php
@@ -74,12 +74,29 @@
 
   <header id="header" role="banner">
     <div id="inner-wrapper">
+    <div id="site-info">
+    <div class="info-contain">	
 	<div id="logo-wrap">
     <?php if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a> 
     <?php endif; ?>
 	</div>
-   
+    
+      <?php if ($site_name || $site_slogan): ?>
+
+        <?php if ($site_name): ?>
+          <h1 class="header__site-name" id="site-name">
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><?php print $site_name; ?></a>
+          </h1>
+        <?php endif; ?>
+
+        <?php if ($site_slogan): ?>
+          <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
+        <?php endif; ?>
+  
+       <?php endif; ?>
+   </div><!--info-contain-->
+      </div><!--siteinfo-->
    
 
 	   <nav id="navigation">
@@ -106,21 +123,11 @@
 
     </nav><!-- /#navigation -->
     <?php print render($page['header']); ?>
+
+
     </div><!--innner-wrapper-->
   </header>
-  <?php if ($site_name || $site_slogan): ?>
 
-        <?php if ($site_name): ?>
-          <h1 class="header__site-name" id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><?php print $site_name; ?></a>
-          </h1>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
-        <?php endif; ?>
-  
-    <?php endif; ?>
   <div id="main">
 
     <div id="content" class="column" role="main">
